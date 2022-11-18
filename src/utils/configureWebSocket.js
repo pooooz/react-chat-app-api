850404a45a1454f7627273f8fe12c88bf004fb24
POZ <antoivans@gmail.com>
2022-11-18 05:15:25 +0300

    Feat: introduce websockets

---
+++ src/utils/configureWebSocket.js
@@ -0,0 +1,54 @@
+import { v4 as uuid } from 'uuid';
+import { Messages } from '../models/messages.js';
+
+const rooms = {};
+
+const leave = (room, connectionId) => {
+  if (!rooms[room][connectionId]) return;
+
+  if (Object.keys(rooms[room]).length === 1) delete rooms[room];
+
+  else delete rooms[room][connectionId];
+};
+
+const join = (room, connectionId, socket) => {
+  if (!rooms[room]) rooms[room] = {};
+  if (!rooms[room][connectionId]) rooms[room][connectionId] = socket;
+};
+
+export const configureWebSocket = (wss) => {
+  wss.on('connection', (ws) => {
+    const connectionId = uuid();
+    ws.on('message', (message) => {
+      const stringedMessage = message.toString();
+      const {
+        type, text, author, chatId,
+      } = JSON.parse(stringedMessage);
+
+      switch (type) {
+        case 'join':
+          join(chatId, connectionId, ws);
+          break;
+        case 'leave':
+          leave(chatId, connectionId);
+          break;
+        case 'message':
+          Object.entries(rooms[chatId])
+            .forEach(([, sock]) => {
+              Messages.create({ text, author, chatId })
+                .then((created) => {
+                  sock.send(JSON.stringify(created));
+                }).catch((error) => {
+                  console.error(error);
+                });
+            });
+          break;
+        default:
+          console.warn(`Type: ${type} unknown`);
+          break;
+      }
+    });
+
+    ws.on('error', (error) => ws.send(error));
+  });
+};
